3890e8d1d993798ef5bb32f090638f4b41ca7f13
amycollendiaye <amycollendiaye103@gmail.com>
2025-11-16 15:06:18 +0000

    message erreur pour le cni

--- app/Http/Requests/CreateCompte.php
+++ app/Http/Requests/CreateCompte.php
@@ -31,7 +31,7 @@ class CreateCompte extends FormRequest
             "client.prenom" => ['required', 'string', 'min:2', 'max:255'],
             "client.adresse" => ['required', 'string', 'min:5', 'max:500'],
             "client.telephone" => ['required', 'string', new \App\Rules\Telephone],
-            "client.cni" => ['required', 'string', new \App\Rules\Cni],
+            "client.cni" => ['required', 'string', new \App\Rules\Cni,'unique:clients,cni'],
             // "client.code_secret" => ['required', 'string', new \App\Rules\CodeSecret],
             "type" => ['required', 'string', Rule::in(['client', 'distributeur'])],
         ];
@@ -56,6 +56,8 @@ class CreateCompte extends FormRequest
             "client.telephone.required" => 'Le numéro de téléphone est obligatoire.',
             "client.telephone.unique" => 'Ce numéro de téléphone existe déjà.',
             "client.cni.required" => 'Le numéro CNI est obligatoire.',
+                "client.cni.unique" => 'Ce cni  existe déjà.',
+
             // "client.code_secret.required" => 'Le code secret est obligatoire.',
             "type.required" => 'Le type de compte est obligatoire.',
             "type.string" => 'Le type de compte doit être une chaîne de caractères.',
